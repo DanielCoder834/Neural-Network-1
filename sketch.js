2cefcd55172b7877142617034313843b02e366c8
Daniel K <dkaplan@aimpa.org>
2021-04-30 13:21:27 -0400

    idk

--- sketch.js
+++ sketch.js
@@ -22,23 +22,35 @@ let training_data = [
 
 function setup() {
     //Test Data
-    // for (let i = 0; i < 50000; i++) {
-    //     let data = random(training_data);
-    //     nn.train(data.inputs, data.targets);
-    // }
-
-    // console.log(nn.feedforward([1,0]));
-    // console.log(nn.feedforward([0,1]));
-    // console.log(nn.feedforward([1,1]));
-    // console.log(nn.feedforward([0,0]));
-    createCanvas(400, 400);
     nn = new NeuralNetwork(2, 2, 1);
+    for (let i = 0; i < 50000; i++) {
+        let data = random(training_data);
+        nn.train(data.inputs, data.targets);
+    }
+
+    console.log(nn.feedforward([1,0]));
+    console.log(nn.feedforward([0,1]));
+    console.log(nn.feedforward([1,1]));
+    console.log(nn.feedforward([0,0]));
+    createCanvas(400, 400);
 }
 
 function draw() {
 
     background(0)
 
-    let data = random(training_data);
-    nn.train(data.inputs, data.outputs);
+    for (let i = 0; i < 1000; i++) {
+        let data = random(training_data);
+        nn.train(data.inputs, data.outputs);
+    }
+
+    let resolution = 10;
+    let cols = width / resolution;
+    let rows = height / resolution;
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            fill(random(255));
+            rect(i*resolution,j*resolution,resolution, resolution)
+        }
+    }
 }
